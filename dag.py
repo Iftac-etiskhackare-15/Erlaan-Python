af0ec68fefd9ead311a1ffee4f902ddf3d221bd8
erlaan <firensonic@sandia-gaming.se>
2016-09-08 10:06:21 +0200

    This is a new Hello world program

---
+++ dag.py
@@ -0,0 +1,11 @@
+class Dog():
+    def __init__(self, name, age, breed):
+        self.name = name
+        self.age = age
+        self.breed = breed
+
+
+hund1=Dog("Gunnar",4,"labradoodle")
+print(hund1.name)
+print(hund1.age)
+print(hund1.breed)
--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,6 @@
 hello = "hello there "
-name = input("Vad heter du? ")
-print (hello.title() + name.title())
+name = input("This is a Hello World program. Plz type \"Hello World!\" ")
+if (name == "Hello World!"):
+    print("Go and fuck your self. This is no Hello World program. This is to advanced!")
+else:
+    print (hello.title() + name.title())
