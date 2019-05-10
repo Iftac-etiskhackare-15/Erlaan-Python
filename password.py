edd3978c78dc82e93b39a7eac35dbc3128bf5cac
Erlaan <erlaan@localhost.localdomain>
2019-05-10 21:27:08 +0200

    I fixed a bug that i notice on a WIndows machine when you just click on the python script and it will generate the password but close the terminal so you can't se the password

--- password.py
+++ password.py
@@ -2,25 +2,29 @@
 
 #Import function
 import random
-
+loop = 1
 #code start
- 
-
-many=int(input("How many password do you want to be generated? "))
-lenght=int(input("How long password you want? "))
-password = ""
-for x in range(0, many):
-    for b in range(0, lenght):
-        rad = random.randint(0, 4)
-        if (rad == 1) :
-            pas = random.choice('abcdefghijklmnopqrstuvwxyz')
-        elif (rad == 2) :
-            pas = random.choice('%&#=+')
-        elif (rad == 3) :
-            pas = random.choice('1234567890')
-        elif (rad == 4) :
-            pas = random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-        
-        password = password + pas
-    print (password)
+while True:
+    many=int(input("How many password do you want to be generated? "))
+    lenght=int(input("How long password you want? "))
     password = ""
+    pas = ""
+    for x in range(0, many):
+        for b in range(0, lenght):
+            rad = random.randint(0, 4)
+            if (rad == 1) :
+                pas = random.choice('abcdefghijklmnopqrstuvwxyz')
+            elif (rad == 2) :
+                pas = random.choice('%&#=+')
+            elif (rad == 3) :
+                pas = random.choice('1234567890')
+            elif (rad == 4) :
+                pas = random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+
+            password = password + pas
+        print (password)
+        password = ""
+    close=input("Do you want to generatet more password? Y/n ")
+
+    if close != "Y" :
+            break
